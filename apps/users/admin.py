ebe1cbb2a595ed30f6ef320d7a994ecb08d1ff78
williamcullen <williamcullen@foxmail.com>
2017-04-12 15:40:15 +0800

    Revert "first"
    This reverts commit 27a838f6d4bf71b9038f1b49bfee12a5fec137c4.

--- apps/users/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# import xadmin
-# from django.db import models
-# xadmin.site.register(models.model)
--- apps/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# _*_ coding:utf-8 _*_
-__author__ = 'Bruse'
-__date__ = '2017-01-19 7:10'
